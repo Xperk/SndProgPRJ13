de28a454f919ee6956d1a759938392c227769a88
Tung <TungLV.B20PT185@stu.ptit.edu.vn>
2023-03-28 22:31:03 +0700

    update

--- Signal.py
+++ Signal.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import resample
+import librosa as librosa
 
 class Signal:
     def __init__(self, data, sr):
         self.data = data
         self.sr = sr
+        self.root = self.data[0];
         self.length = len(self.data)
 
     def plot(self, start_sec=None, end_sec=None):
@@ -28,4 +31,34 @@ class Signal:
     def flipp_time(self):
         self.data = np.flip(self.data)
 
+    def multiply(self,other):
+        self.data = np.multiply(self.data,other.data)
 
+    def add(self,other):
+        self.data = np.add(self.data,other.data)
+
+    def divide(self,other):
+        self.data = np.divide(self.data,other.data)
+
+    def subtract(self,other):
+        self.data = np.divide(self.data,other.data)
+
+    def compress(self, threshold):
+        compressed_data = np.sign(self.data) * np.maximum(0, np.abs(self.data) - threshold)
+        return Signal(compressed_data, self.sr)
+
+
+    def resample(self,n):
+        self.data = resample(self.data, len(self.data)*n)
+
+    def convolve(self,other):
+        self.data = np.convolve(self.data, other.data,mode='full')
+
+    def correlate(self,other):
+        self.data = np.correlate(self.data,other.data,mode='full')
+
+    def energy(self):
+        return np.sum(np.square(np.abs(self.data)))
+
+    def power(self):
+        return np.mean(np.square(np.abs(self.data)))
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from Signal import Signal
-import numpy as np
 if __name__ == '__main__':
     data1 = [1,4,5,6,1,3]
-    sr1 = 44100
-    signal1 = Signal(data1, sr1)
-    signal1.plot()
+    sr = 44100
+    signal1 = Signal(data1, sr)
+
+    data2 = [7,5,3,5,9,2,1,4,5,6,1,3]
+    signal2 = Signal(data2,sr)
+
+    data3 = [12, 10, 6, 10, 18, 4, 2, 4, 10, 6, 2, 6]
+    signal3 = Signal(data3, sr)
 
     # signal1.shift_time(10)
 
-    signal1.flipp_time()
+    # signal1.flipp_time()
+
+    # signal1.multiply(signal2)
+
+    # signal3.resample(2)
+
+    # signal1.convolve(signal2)
+
+    signal1.compress(0.5)
+
+    # energy = signal1.energy()
+
+    # print(energy)
     signal1.plot()
 
-    # data2 = [7,5,3,5,9,2]
-    # src2 = 44100
-    # signal2 = Signal(data2,src2)
-    # signal2.plot()
+
 
